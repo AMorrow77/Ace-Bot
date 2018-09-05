7424ac88d92427a2127930296ac967aa909d0bca
AMorrow77 <arthur.morrow525@gmail.com>
2018-09-05 14:20:48 -0400

    Add files via upload

---
+++ ACE-Bot-Dev-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=ace-bot-dev-Web-Deploy.pubxml -p:Password=BEjuptCgxclrySR8vah9psoQ55o9LleqLFi3Pb8K0NRpW8mofeJxZmFoGaKQ
+
