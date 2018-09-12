9d13a28313a669b7237727af7f7bb6f68936f12e
Arthur Morrow <arthur.morrow525@gmail.com>
2018-09-12 13:15:36 -0400

    Query Copy

---
+++ ACE-Bot-Dev-src/Services/SPDataService.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.SharePoint.Client;
+using SP = Microsoft.SharePoint.Client;
+
+namespace Microsoft.SDK.SharePointServices.Samples
+{
+    class RetrieveListItems
+    {
+        static void Main()
+        {
+            string siteUrl = "https://plusdemo01.sharepoint.com";
+
+            ClientContext clientContext = new ClientContext(siteUrl);
+            SP.List oList = clientContext.Web.Lists.GetByTitle("Mock Projects");
+
+            CamlQuery camlQuery = new CamlQuery();
+            camlQuery.ViewXml = "<View><Query><Where><Geq><FieldRef Name='Title'/>" +
+                "<Value Type='Text'>Arconic</Value></Geq></Where></Query><RowLimit>100</RowLimit></View>";
+            ListItemCollection collListItem = oList.GetItems(camlQuery);
+
+            clientContext.Load(collListItem);
+
+            clientContext.ExecuteQuery();
+
+            foreach (ListItem oListItem in collListItem)
+            {
+                Console.WriteLine("ID: {0} \nTitle: {1} \nHours: {2}", oListItem.Id, oListItem["Title"], oListItem["Hours"]);
+            }
+        }
+    }
+}
